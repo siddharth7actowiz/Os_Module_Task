5fa4ad0467778435df2c510912572cfe21be2c15
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-02-13 14:33:11 +0530

    corrected mistake-for loop condition and corrected todays ddate

--- folders_title_as_date.py
+++ folders_title_as_date.py
@@ -1,25 +1,29 @@
 import os
 from datetime import datetime, timedelta,date
-from os.path import exists
 
+from calendar import *
 #Taking User Input
-current_date=input("Enter date to create folders of remaining days in year in YYYY-M-D:")
+current_date=input("Enter date to create folders of remaining days in year in YYYY-MM-DD:")
 
 def create_folders_date_name(current_date):
     # getting todays_date from input
     todays_date=datetime.strptime(current_date,"%Y-%m-%d")
 
     #for current day nuber of year
-    today_timetuple=datetime.now().timetuple()
+    today_timetuple=todays_date.timetuple()
     curr_day_num=today_timetuple.tm_yday
     rem_days=365-curr_day_num
 
-        for i in range(1,rem_days+1):
+    #checking leap yaer
+    if isleap(today_timetuple.tm_year):
+        rem_days+=1
+
+    for i in range(1,rem_days+1):
         folder_date=todays_date + timedelta(days= i)
         # converts datetime objet to string
         folder_name=datetime.strftime(folder_date,"%Y-%m-%d")
-        
         # creating repositories
+        print(folder_name)
         os.mkdir(f"D:/Siddharth/date_name_folder/date_folders/{folder_name}")
 
 #function call
