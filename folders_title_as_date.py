e5727a6ea28c8b600d9e34accd48c5db401e87b5
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-02-13 19:08:26 +0530

    update-code for creating files inside folders

--- folders_title_as_date.py
+++ folders_title_as_date.py
@@ -1,11 +1,17 @@
+
 import os
-from datetime import datetime, timedelta,date
+
+from datetime import datetime, timedelta
 
 from calendar import *
+
+
 #Taking User Input
 current_date=input("Enter date to create folders of remaining days in year in YYYY-MM-DD:")
+#to create files extensions
+extensions=[".txt",".html",".py",".js"]
 
-def create_folders_date_name(current_date):
+def create_files_inside_folders_date_name(current_date):
     # getting todays_date from input
     todays_date=datetime.strptime(current_date,"%Y-%m-%d")
 
@@ -13,6 +19,7 @@ def create_folders_date_name(current_date):
     today_timetuple=todays_date.timetuple()
     curr_day_num=today_timetuple.tm_yday
     rem_days=365-curr_day_num
+
 
     #checking leap yaer
     if isleap(today_timetuple.tm_year):
@@ -24,7 +31,25 @@ def create_folders_date_name(current_date):
         folder_name=datetime.strftime(folder_date,"%Y-%m-%d")
         # creating repositories
         print(folder_name)
-        os.mkdir(f"D:/Siddharth/date_name_folder/date_folders/{folder_name}")
+        os.mkdir(f"D:/Siddharth/files _with_dt_name_in_dt_folder/{folder_name}")
+
+        #monthrange functon to find total days in a moth
+        last_day_month=monthrange(folder_date.year,folder_date.month)[1]
+
+        #current day for file
+        day_in_file=(last_day_month-folder_date.day)+1
+
+        #creates datetime object that will be rev for users current date
+        rev_date=datetime(folder_date.year,folder_date.month,day_in_file)
+
+        #formatting datetime object
+        file_name=rev_date.strftime("%Y-%m-%d")
+
+        for ext in extensions:
+            file_path = f"D:/Siddharth/files _with_dt_name_in_dt_folder/{folder_name}/{file_name}{ext}"
+            with open(file_path, "w") as f:
+                f.write(f"{ext}")
+
 
 #function call
-create_folders_date_name(current_date)
+create_files_inside_folders_date_name(current_date)
