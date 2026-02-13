7a286a0dc1c538645bd4a0a29a4e529906c170c4
Siddharth Doshi <Siddharth.Doshi@act.local>
2026-02-13 12:11:58 +0530

    updated code with passing user inp as arg in code

--- folders_title_as_date.py
+++ folders_title_as_date.py
@@ -1,15 +1,15 @@
 import os
 from datetime import datetime, timedelta,date
 
-
-def create_folders_date_name():
-    # getting todays_date
-    todays_date=date(2026,2,13)
+current_date=input("Enter date to create folders of remaining days in year in YYYY-M-D:")
+def create_folders_date_name(current_date):
+    # getting todays_date from input
+    todays_date=datetime.strptime(current_date,"%Y %m %d")
     # for current day nuber of year
     today_timetuple=datetime.now().timetuple()
 
 
-    for i in range(1,320):
+    for i in range(1,322):
         folder_date=todays_date + timedelta(days= i)
         # converts datetime objet to string
         folder_name=datetime.strftime(folder_date,"%Y %m %d")
@@ -17,4 +17,4 @@ def create_folders_date_name():
         os.mkdir(f"D:/Siddharth/os_module_task/{folder_name}")
 
 #function call
-create_folders_date_name()
+create_folders_date_name(current_date)
