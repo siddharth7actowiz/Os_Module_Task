063609e7df9b81dc801f7d89be2aef1006ef36a3
Siddharth Doshi <Siddharth.Doshi@act.local>
2026-02-13 13:08:08 +0530

    updated code

--- folders_title_as_date.py
+++ folders_title_as_date.py
@@ -7,19 +7,19 @@ current_date=input("Enter date to create folders of remaining days in year in YY
 
 def create_folders_date_name(current_date):
     # getting todays_date from input
-    todays_date=datetime.strptime(current_date,"%Y %m %d")
+    todays_date=datetime.strptime(current_date,"%Y-%m-%d")
 
     #for current day nuber of year
     today_timetuple=datetime.now().timetuple()
     curr_day_num=today_timetuple.tm_yday
     rem_days=365-curr_day_num
 
-    for i in range(rem_days):
+    for i in range(rem_days+2):
         folder_date=todays_date + timedelta(days= i)
         # converts datetime objet to string
-        folder_name=datetime.strftime(folder_date,"%Y %m %d")
+        folder_name=datetime.strftime(folder_date,"%Y-%m-%d")
         # creating repositories
-        os.makedirs(f"D:/Siddharth/os_module_task/{folder_name}",exist_ok=True)
+        os.mkdir(f"D:/Siddharth/date_name_folder/date_folders/{folder_name}")
 
 #function call
 create_folders_date_name(current_date)
