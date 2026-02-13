6ab740ea035294498309da7cec1a59fd53ff8340
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-02-13 13:55:58 +0530

    Update folders_title_as_date.py

--- folders_title_as_date.py
+++ folders_title_as_date.py
@@ -14,10 +14,11 @@ def create_folders_date_name(current_date):
     curr_day_num=today_timetuple.tm_yday
     rem_days=365-curr_day_num
 
-    for i in range(rem_days+1):
+        for i in range(1,rem_days+1):
         folder_date=todays_date + timedelta(days= i)
         # converts datetime objet to string
         folder_name=datetime.strftime(folder_date,"%Y-%m-%d")
+        
         # creating repositories
         os.mkdir(f"D:/Siddharth/date_name_folder/date_folders/{folder_name}")
 
